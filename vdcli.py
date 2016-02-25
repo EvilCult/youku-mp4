859366dfe10149dc5b4de14cf4f1025355132c6d
U-IBM194-P16TCDD\liuyug <liuyug@gmail.com>
2016-02-25 10:37:36 +0800

    add ffmepg merge

--- vdcli.py
+++ vdcli.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
 # -*- encoding:utf-8 -*-
 
+import argparse
+import subprocess
+import os
+import os.path
+import shutil
+import tempfile
+from urllib import urlretrieve
+
+from progressbar import ProgressBar, Percentage, Bar, FileTransferSpeed, Timer
+
 from Module import youkuClass
 from Module import tudouClass
 from Module import sohuClass
@@ -33,31 +43,25 @@ def get_parser(url):
     return getClass
 
 
-def download_file(url, title=None):
+def download_file(url, title, info=None):
     def download_process(count, bsize, tsize):
         if pbar.start_time is None:
-            pbar.start(tsize)
+            pbar.start(tsize + bsize - tsize % bsize)
         dsize = count * bsize
         pbar.update(dsize if dsize < tsize else tsize)
 
-    import shutil
-    from urllib import urlretrieve
-
-    from progressbar import ProgressBar, Percentage, Bar, FileTransferSpeed, Timer
-
     pbar = ProgressBar(
-        widgets=[Percentage(), Bar(), Timer(), ' ', FileTransferSpeed()],
+        widgets=['[%s]' % info, Percentage(), Bar(), Timer(), ' ', FileTransferSpeed()],
     )
-    tmp, header = urlretrieve(url, reporthook=download_process)
+    tmpfile, header = urlretrieve(url, reporthook=download_process)
     pbar.finish()
-    ext = header.getsubtype().split('-')[1]
-    filename = '%s.%s' % (title, ext)
-    shutil.copyfile(tmp, filename)
-    return filename
-
+    tmp_dir = os.path.dirname(tmpfile)
+    ext = os.path.splitext(tmpfile)[1]
+    outfile = os.path.join(tmp_dir, title + ext)
+    shutil.copyfile(tmpfile, outfile)
+    return outfile
 
 if __name__ == '__main__':
-    import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('--quality', default='720p', help='Quality: 720p, 480p, 360p')
     parser.add_argument('--no-download', action='store_true', help='No download, only show download url.')
@@ -71,9 +75,43 @@ if __name__ == '__main__':
 
     if urlList['stat'] == 0 and urlList['msg']:
         urls = urlList['msg']
-        for x in range(len(urls)):
+        dfiles = []
+        title = 'title'
+        total = len(urls)
+        tl = len(str(total))
+        tfmt = '%%0%dd' % tl
+        for x in range(total):
             if args.no_download:
-                print('%s: %s' % (x, urls[x]))
+                print((tfmt + ': %s') % (x + 1, urls[x]))
                 continue
-            filename = download_file(urls[x])
-            print('Save as %s' % filename)
+            filename = download_file(
+                urls[x],
+                title=('%s.' + tfmt + '.part') % (title, x + 1),
+                info=(tfmt + '/%s') % (x + 1, total),
+            )
+            dfiles.append(filename)
+        if dfiles:
+            ext = os.path.splitext(dfiles[0])[1]
+            outfile = title + ext
+            if len(dfiles) == 1:
+                shutil.move(dfiles[0], outfile)
+                print('Save as %s' % outfile)
+            else:
+                ftmp = tempfile.NamedTemporaryFile(prefix=title, delete=False)
+                tmp_name = ftmp.name
+                ftmp.write('\n'.join(['file "%s"' % df for df in dfiles]))
+                ftmp.close()
+                merge = ['ffmpeg']
+                merge.append('-f concat')
+                merge.append('-i %s' % tmp_name)
+                merge.append('-c copy')
+                merge.append(outfile)
+                ret = subprocess.call(merge)
+                if ret == 0:
+                    os.remove(tmp_name)
+                    for df in dfiles:
+                        os.remove(df)
+                    print('Save as %s' % outfile)
+                else:
+                    print('FFMPEG error: ' + ' '.join(merge))
+                    print('Video part:', dfiles)
